74b6f0d51c16d005df8c5040be93b229243a553d
Michael Wu <wuxiaohua1011@gmail.com>
2023-05-29 22:09:55 -0700

    lite: remove network

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, {useState, useCallback, useEffect} from 'react';
+import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 var Carousel = require('react-responsive-carousel').Carousel;
@@ -7,59 +6,39 @@ var Carousel = require('react-responsive-carousel').Carousel;
 import Main from '../layouts/Main';
 
 const PhotoGallery = () => {
-    const [images, setImages] = useState([]); 
-    const [currentImage, setCurrentImage] = useState(0);
-    const [isViewerOpen, setIsViewerOpen] = useState(false);
-    const [isInitialRender, setIsInitialRender] = useState(true);
+  const images = [
+    "1ePHIQP1o8-dMd_0k3Tps2LhtLGsCSJTb",
+    "13k_YOuxTjjVN_kITORxryuyGRu8K_f2y",
+    "1L11Cp_dQRMa55NgRZx5mE22JXnxOxQQw",
+    "1uu-ASMy2u2weMaXGwmbfta6xKIzktH0E",
+    "1PTXtIYdgDaAwKr-_3UNq13EsLORQUq1I",
+    "1UwXa_OJ37AdrEEdPZBY-8czMONbFA5my",
+    "1fDidhtYvAKnDiKe2vty4OJcKwCS5TBwf",
+    "1M7P8bt6AZc66cC5gCjSooYCTpfkPB9yn",
+    "1027gRMG0hn35NV2aJE7A2pFX6laorYVo",
+    "1ePHIQP1o8-dMd_0k3Tps2LhtLGsCSJTb",
+    "1J3HWtWuL5CjSYPEskQw_R-1Y-f9QiKnV",
+    "1I3EUbKQQsak1NpXi3B8RKIH4T0JISyYt",
+    "16OEv_nPmcar5Gb1RNB6HXccPd2_yJVbs",
+    "1hiZkeEhi477pyPyKJshGKgtU2pgedac3",
+    "12ChWMNMfnv-OP_5jmXMYJQ-NGNqPUO3Q"
+  ];
 
-
-    useEffect(() => {
-        if (isInitialRender) {
-          setIsInitialRender(false);
-          var new_images = [] 
-          var host = process.env.REACT_APP_SERVER_URL
-          var port = process.env.REACT_APP_SERVER_PORT
-          var version = "v3";
-          var base_url = "http://" + host+":"+port + "/" + version;
-          console.log("Base url: "+base_url);
-
-          axios.get(base_url+"/photos/list",  {
-              timeout: 10000,
-              headers: {
-                  'Content-Type': 'application/json',
-                  Accept: 'application/json'
-              }
-          }).then(resp => {
-                for (var i = 0; i < resp.data.photos.length; i++) {
-                  new_images.push(resp.data.photos[i]) 
-                }
-                setImages(new_images);
-              });
-        }
-    }, [images, isInitialRender])
-    return (
-      <div>      
-         <Carousel>
-            {images.map((photoName_gdriveFileID) => 
-              <div key={photoName_gdriveFileID.photo_fname}>
-                <img src={"https://drive.google.com/uc?export=view&id="+photoName_gdriveFileID.gdrive_file_id}/>
-                <p className="legend">{photoName_gdriveFileID.photo_fname}</p>
-              </div>
-              )
-            }
-            </Carousel>
-      </div>
-
-    );
-}
+  return (
+    <div>
+      <Carousel>
+        {images.map((image_id, i) => (
+          <div key={i}>
+            <img src={"https://drive.google.com/uc?export=view&id=" + image_id} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 const Gallery = () => (
-
-  <Main
-    title="Gallery"
-    description="Michael's Photography Gallery"
-  >
-  
+  <Main title="Gallery" description="Michael's Photography Gallery">
     <PhotoGallery />
   </Main>
 );
