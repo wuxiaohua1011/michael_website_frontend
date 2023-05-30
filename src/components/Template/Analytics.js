ba4b73c8773acd9e6ff0b5e2fa7cc50c40597614
Michael Wu <wuxiaohua1011@gmail.com>
2023-05-29 22:47:53 -0700

    lite: updated analytics

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,22 +1,20 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
+import ReactGA from "react-ga4";
 
 const { NODE_ENV, REACT_APP_GA_TRACKING_ID } = process.env;
 
-if (NODE_ENV === 'production') {
-  ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
-}
+// if (NODE_ENV === 'production') {
+//   ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
+// }
 
 const Analytics = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     if (NODE_ENV === 'production') {
-      ReactGA.set({
-        page: pathname,
-      });
-      ReactGA.pageview(pathname);
+        ReactGA.initialize(REACT_APP_GA_TRACKING_ID);
     }
   }, [pathname]);
 
